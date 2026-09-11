3b04b68e0eadec5ec6290a026896a75ffa8e58f9
Vihari Ponugoti <vponugoti0-png@users.noreply.github.com>
2026-09-11 16:29:50 +0000

    chore: remove scaffold leftovers and tidy SearchBox
    Drop unused create-next-app public SVGs and agent stub docs.

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 import { Search } from "lucide-react";
 
